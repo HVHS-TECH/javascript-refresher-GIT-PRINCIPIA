8c6d064f6849d0fa0ce3e09172384d07b08f3bf2
PRINCIPIA <AlexMindstorms@hotmail.com>
2026-02-01 22:03:53 +0000

    Day 1 Commit :: Learn GITHUB Codespaces

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Congratulations!\nYou got the script running.")
+var DOC_input = document.getElementById("input_text");
+var name = "...";
+function RunAlert() {
+    console.log("RunAlert() :: DOC_input is " + (DOC_input === null) ? "" : "not " + "null");
+    name = DOC_input.value;
+    alert("RunAlert() :: RunAlert() has been called\nHello " + name + "!");
+}
