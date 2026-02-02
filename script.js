1490744885eea7741e562562f12552f52f076a15
PRINCIPIA <AlexMindstorms@hotmail.com>
2026-02-02 03:24:53 +0000

    Fixed get input

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-var DOC_input = document.getElementById("input_text");
-var name = "...";
+
+
 function RunAlert() {
-    console.log("RunAlert() :: DOC_input is " + (DOC_input === null) ? "" : "not " + "null");
-    name = DOC_input.value;
-    alert("RunAlert() :: RunAlert() has been called\nHello " + name + "!");
+    var DOC_input = document.getElementById("input_text");
+    var text = "...";
+    text = DOC_input.value;
+    console.log("RunAlert() :: RunAlert() has been called");
+    console.log("RunAlert() :: text = " + text);
+    alert(text);
 }
