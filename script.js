f94ee1d79959da8290e74892eea3e92b732db048
= <=>
2026-02-03 11:51:14 +1300

    Comments

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Sets 'OnLoad()' to run on window load
 window.addEventListener("onLoad", OnLoad)
 
-var DOC_input;
-var DOC_welcome_message
+var DOC_input; //The input box for changing title and header
+var DOC_welcome_message; //The welcome message (header)
 
 //Called on window load
 function OnLoad() {
@@ -13,12 +13,20 @@ function OnLoad() {
 //Changes the title to 'input_text'. If 'input_text' is "", it sets the title to "..."
 function ChangeTitle() {
     var text = "..."; //Empty title
+
     if (DOC_input.value != "") text = DOC_input.value;
+
+    //Logging
     console.log("RunAlert() :: RunAlert() has been called");
     console.log("RunAlert() :: text = " + text);
+
+    //Set the title of the document to 'text'
     document.title = text;
    
+    //Set the welcome message text to use 'text'
     DOC_welcome_message.innerText = "Welcome to the " + text + " page!";
-    DOC_input.value = ""; //Reset the text in 'DOC_input'
+
+    //Reset the text in 'DOC_input'
+    DOC_input.value = ""; 
 }
 
